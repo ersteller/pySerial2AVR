bdf3978ee8ed8e3ec54f36f8d3f343f93391f4b6
Jan <Jan@Jans-QPC>
2015-11-19 23:59:43 +0100

    added command functions in headerfile

--- uart/uart/remo.h
+++ uart/uart/remo.h
@@ -13,16 +13,36 @@
 #include <avr/io.h>
 #include <string.h>
 #include "uart.h"
+#include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 
+volatile uint8_t fOverflow;
+
+typedef struct LEVELS_Ttag{
+	uint8_t		fAH;
+	uint8_t		fAL;
+	uint8_t		fBH;
+	uint8_t		fBL;
+	uint8_t		fCH;
+	uint8_t		fCL;
+}LEVELS_T;
+
 uint8_t instructionHandler(uint8_t c);
+uint8_t HandlerCommand(uint8_t c);
+uint8_t HandlerBinary(uint8_t c);
+void setHandler(uint8_t (*pfnH)(uint8_t));
 
 uint8_t setValAddr(uint16_t addr, uint8_t val);
-uint8_t getValAddr(uint16_t addr);
+uint32_t getValAddr(uint16_t addr, uint8_t len);
+void* getMalloc(uint16_t len);
+void setFree(void* pvMem);
+void* setPWMBuffer(char* pbInstruction);
 
-
-
-
-
+void PWMHandler();
+void lookupPWM();
+void setPinLevles(volatile uint8_t ubPortLevel);
+void setOutputPinsAddr(char* szParam);
+void setAmplidudeAndSpeed(char* szParam);
+void setPhaseShift(char* szParam);
 
 #endif /* REMO_H_ */
